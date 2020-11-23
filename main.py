18322b231a11601abe7f27b39b06b7649956fabe
Fabrice Reigner <fabricereigner49@msn.com>
2020-11-23 11:51:35 +0100

    ajout de plusieurs valeur

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-priceHT = int(input('indiquer une valeur :'))
-quantity = int(input('indiquer une quantiter :'))
-if priceHT <=0:
-    priceHT = int(input('indiquer une valeur :'))
-elif quantity <=0:
-    quantity = int(input('indiquer une quantiter :'))
-else:
-    print("valeur correcte")
+i = int(input('indiquer le nb de produit :'))
+produit = [i]
+for i in range(i):
+    priceHT = float(input('indiquer une valeur :'))
+    if priceHT <=0:
+        priceHT = float(input('indiquer une nouvelle valeur :'))
+    quantity = int(input('indiquer une nouvelle quantité :'))
+    if quantity <=0:
+        quantity = int(input('indiquer une quantité :'))
 
+    priceQ = priceHT * quantity
+    Tva = 0.20
+    remise = 0.05
+    priceTva = priceQ * Tva + priceQ
 
-priceQ = priceHT * quantity
-Tva = 0.20
-remise = 0.05
-priceTva = priceQ*Tva + priceQ
+    if priceTva >= 200:
+        discount = priceTva - (priceTva * remise)
+        priceTva = discount
 
-if priceTva >= 200:
-    discount = priceTva - (priceTva*remise)
-    priceTva = discount
+    print(priceTva)
 
-print(priceTva)
