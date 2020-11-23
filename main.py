251971d4c4a1e700af3b9982e0f224aae7a7c050
Loic GUILLOIS <contact@loicguillois.fr>
2020-11-23 14:41:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 i = int(input('indiquer le nb de produit :'))
-produit = [i]
+# déclarer un dictionnaire à la place du dictionnaire produits
+# produit = {"id": 1, "nom":"banane", "prix": 4, ...}
+
+# afficher les produits
+# TODO
+
+produit = [i] 
 for i in range(i):
+    # remplacer cette question par la question "Quel produit à ajouter ?"
     priceHT = float(input('indiquer une valeur :'))
     if priceHT <=0:
         priceHT = float(input('indiquer une nouvelle valeur :'))
@@ -9,6 +16,7 @@ for i in range(i):
     if quantity <=0:
         quantity = int(input('indiquer une quantité :'))
 
+    # prendre en compte le prix figurant dans le dictionnaire
     priceQ = priceHT * quantity
     Tva = 0.20
     remise = 0.05
